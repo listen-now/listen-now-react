4fd0daf2d9861bde4eda8657da82b1a4b7ff529c
热爱 <304192604@qq.com>
2018-10-10 23:56:09 +0800

    feat(添加 Logo、Search、Panel 以及 LnIcon 组件):

---
+++ src/components/Panel/NavItem/NavItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NavItem.less';
+import LnIcon from '../../../packages/LnIcon';
+
+/**
+ * NavItem
+ */
+export default class NavItem extends Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
+
+  static defaultTypes = {};
+
+  render() {
+    const { item } = this.props;
+    return (
+      <li className="nav-item">
+        <a href={item.url} className="nav-item__link">
+          <LnIcon type={item.iconType} size={20} className="nav-item__icon" />
+          <span className="nav-item__text">{item.text}</span>
+        </a>
+      </li>
+    );
+  }
+}
